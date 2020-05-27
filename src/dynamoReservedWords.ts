cd44194a0dab9fd49e320b930196d44ceddd9366
Eric Moore <eric.d.moore@gmail.com>
2020-05-27 12:33:12 -0500

    Still testing out git hooks

--- src/dynamoReservedWords.ts
+++ src/dynamoReservedWords.ts
@@ -1,8 +1,10 @@
-import { brotliDecompressSync } from 'zlib'
+// import {fetch} from 'node-fetch'
 
-export const reservedWords = (fs:fsLike, path: pathLike) => {
-  const fBuff = fs.readFileSync(path.resolve('./src/reserved.txt.br'))
-  const bufOpen = brotliDecompressSync(fBuff)
+export const reservedWords = (fs:fsLike, path: pathLike, decompressSync: IbrotliDecompressSync) => {
+  const compressedBuff = fs.readFileSync(path.resolve('./src/reserved.txt.br'))
+  // const compressedBuff = fetch('https://github.com/federalies/dynamoco/blob/master/utils/reserved.txt.br?raw=true') //=  but this is a promise
+
+  const bufOpen = decompressSync(compressedBuff)
   const words = bufOpen.toString().split('\n')
   return words.reduce((p, w) => ({ ...p, [w]: true }), {})
 }
@@ -16,3 +18,25 @@ interface fsLike{
 interface pathLike{
     resolve(...pathSegments: string[]): string;
 }
+
+interface BrotliOptions {
+  /**
+   * @default constants.BROTLI_OPERATION_PROCESS
+   */
+  flush?: number;
+  /**
+   * @default constants.BROTLI_OPERATION_FINISH
+   */
+  finishFlush?: number;
+  /**
+   * @default 16*1024
+   */
+  chunkSize?: number;
+  params?: {
+      /**
+       * Each key is a `constants.BROTLI_*` constant.
+       */
+      [key: number]: boolean | number;
+  };
+}
+type IbrotliDecompressSync = (buf: string | ArrayBuffer | Buffer, options?: BrotliOptions) => Buffer
